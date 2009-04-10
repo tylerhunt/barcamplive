cb80024d9ec9b716d48ec73b50c38afc6472b71e
Joshua Blount <jblount@r2d2.(none)>
2009-04-10 13:10:48 -0400

    moved changes from bzr repository so I could give git a fair shake

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+window.onload = function() {
+  setTimeout(function(){window.scrollTo(0, 1);}, 100);
+  }
+
