0ca0413c89663e79d151bebcf32726bc3fb6283b
Tyler Hunt <tyler@tylerhunt.com>
2009-04-10 10:19:54 -0400

    Updated session key.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_tmp_session',
+  :key         => '_barcamplive_session',
   :secret      => '65c5e35adefa7ab5751ae43be6dea495f7e993ef42d6356f3816520f6266f7c011e6a9a96602dbb349a0e6b67fefbc902f01fd1385b02b746057da39e3a33f3a'
 }
 
