18fd04841cac9ffe4a4ebf0733d748b2e5df037d
Tyler Hunt <tyler@tylerhunt.com>
2009-04-13 11:20:12 -0400

    Added navigation anchors and removed JavaScript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,2 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
-window.onload = function() {
-  setTimeout(function() { window.scrollTo(0, 1); }, 100);
-}
